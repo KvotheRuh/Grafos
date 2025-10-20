3bba7238c4fff9888676159d63d8b5495a58c1f4
Reshi <duuhmorciani@gmail.com>
2025-10-19 23:05:41 -0300

    Adiciona os metodos de busca em profundidade, largura e de distancia, adiciona os metodos para contar a quantidade de saida e entrada de cada vertice e corrige o algoritmo de dijkstra.

--- AtividadeDois/src/ListaAdjacencia.java
+++ AtividadeDois/src/ListaAdjacencia.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ListaAdjacencia {
    public Vertice[] vertices;
    public int quantidadeVertices;
@@ -51,35 +56,268 @@ public class ListaAdjacencia {
       }
    }
 
-   public void adjacentes (int vertice) {
+   public int[] adjacentes (int vertice, int[] adj) {
       Vertice origem =  vertices[vertice];
       int contador = 0;
       Aresta atual = origem.inicio;
-      String concatenaAjacentes = "";
 
       if (vertice < 0 || vertice >= quantidadeVertices){
          System.out.println("Vértice inválido.");
-         return;
+         return null;
       }
+
       if (origem == null){
          System.out.println("O vértice não possui adjacentes.");
-         return;
+         return null;
       }
 
       while (atual != null) {
-         Vertice destino = vertices[atual.destino];
-         concatenaAjacentes +=  destino.rotulo + "(peso " + atual.peso + ")";
-         atual = atual.proximo;
+         adj[contador] = atual.destino;
          contador++;
+         atual = atual.proximo;
 
-         if (atual != null) {
-            concatenaAjacentes +=  ", ";
+      }
+
+      System.out.print("\nAdjacente de " + origem.rotulo + ": ");
+      if (contador != 0){
+         for (int i = 0; i < contador; i++) {
+            System.out.print(vertices[adj[i]].rotulo);
+            if (i < contador - 1){
+               System.out.print(", ");
+            }
+         }
+      } else {
+         System.out.println("O vértice não possui adjacentes!!!");
+      }
+
+      System.out.printf("\nQuantidade de adjacentes de %s: %d\n", origem.rotulo, contador);
+      System.out.println(" ");
+
+      return adj;
+   }
+
+
+   public void saida () {
+      Map<String, Integer> contadorSaida = new HashMap<>();
+
+      for (int i = 0; i < quantidadeVertices; i++) {
+         Vertice origem = vertices[i];
+         int contador = 0;
+
+         if (origem != null) {
+            Aresta atual = origem.inicio;
+
+            if (atual == null) {
+               contadorSaida.put(origem.rotulo,contador);
+               continue;
+            }
+
+            while (atual != null) {
+               contador++;
+               contadorSaida.put(origem.rotulo, contador);
+               atual = atual.proximo;
+
+            }
+         }
+      }
+      System.out.println("\nOs 20 vértices com maior grau de saída: ");
+      contadorSaida.entrySet().stream()
+              .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+              .limit(20)
+              .forEach(entry -> System.out.println(entry.getKey() + " -> " + entry.getValue()));
+   }
+
+
+   public void entrada() {
+      Map<String, Integer> contadorEntrada = new HashMap<>();
+
+      for (int i = 0; i < quantidadeVertices; i++){
+         Vertice origem = vertices[i];
+
+         if(origem != null){
+            contadorEntrada.put(origem.rotulo, 0);
          }
       }
 
-      System.out.printf("\nAdjacente de %s: %s\n", origem.rotulo, concatenaAjacentes);
-      System.out.printf("Quantidade de adjacentes de %s: %d\n", origem.rotulo, contador);
-      System.out.println(" ");
+      for (int i = 0; i < quantidadeVertices; i++) {
+         Vertice origem = vertices[i];
+         int contador = 0;
+
+         if (origem != null) {
+            Aresta atual = origem.inicio;
+
+            if (atual == null) {
+               continue;
+            }
+
+            while (atual != null && atual.destino != -1) {
+               Vertice destino = vertices[atual.destino];
+
+               if (contadorEntrada.get(destino.rotulo) != null) {
+                  contador = contadorEntrada.get(destino.rotulo);
+               }
+
+               contador++;
+               contadorEntrada.put(destino.rotulo, contador);
+
+               atual = atual.proximo;
+            }
+         }
+      }
+
+      System.out.println("\nOs 20 vértices com maior grau de entrada: ");
+      contadorEntrada.entrySet().stream()
+              .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+              .limit(20)
+              .forEach(entry -> System.out.println(entry.getKey() + " -> " + entry.getValue()));
+   }
+
+   public List<Integer> distancia(int inicio, int numArestas, List<Integer> visitados) {
+
+      visitados.add(inicio);
+      List<Integer> resultado = new ArrayList<>();
+
+      System.out.println("Pilha distância: " + visitados);
+      System.out.println("--------------------------");
+
+      if (numArestas == 0) {
+         resultado.add(inicio);
+         visitados.remove(visitados.size() - 1);
+         return resultado;
+      }
+
+      Vertice origem = vertices[inicio];
+      Aresta atual = origem.inicio;
+
+      while (atual != null) {
+         if (!visitados.contains(atual.destino)) {
+            List<Integer> caminho = distancia(atual.destino, numArestas - 1, visitados);
+            if (caminho != null) {
+               for (int valor : caminho){
+                  if (resultado.contains(valor)){
+                     resultado.add(valor);
+                  }
+               }
+            }
+         }
+
+         atual = atual.proximo;
+
+      }
+
+      visitados.remove(visitados.size() - 1);
+
+      if (visitados.isEmpty()) {
+         System.out.print("\nOs indivíduos que estão a distância de " + numArestas +  " arestas de " + origem.rotulo + " são: ");
+         for (int i = 0; i < resultado.size(); i++) {
+            System.out.print(vertices[resultado.get(i)].rotulo);
+            if (i < resultado.size() - 1) {
+               System.out.print(", ");
+            }
+         }
+
+         System.out.println("\n");
+      }
+
+      return resultado;
+   }
+
+
+   public List<Integer> profundidade(int inicio, int destino, List<Integer> visitados) {
+
+      visitados.add(inicio);
+
+      System.out.println("Pilha: " + visitados);
+      System.out.println("--------------------------");
+
+      if (inicio == destino) {
+         System.out.print("\nBusca por profundidade caminho: ");
+         for (int i = 0; i < visitados.size(); i++) {
+            System.out.print(vertices[visitados.get(i)].rotulo);
+            if (i < visitados.size() - 1) {
+               System.out.print(", ");
+            }
+         }
+
+         System.out.println("\n");
+
+         return visitados;
+
+      } else {
+         Vertice origem = vertices[inicio];
+         Aresta atual = origem.inicio;
+
+         while (atual != null) {
+            if (!visitados.contains(atual.destino)) {
+               List<Integer> caminho = profundidade(atual.destino, destino, visitados);
+               if (caminho != null) {
+                  return caminho;
+               }
+            }
+
+            atual = atual.proximo;
+         }
+
+         visitados.remove(visitados.size() - 1);
+
+         if (visitados.isEmpty()) {
+            System.out.printf("Nenhum caminho encontrado entre %s e %s \n", vertices[inicio].rotulo, vertices[destino].rotulo);
+         }
+
+         return null;
+      }
+   }
+
+
+   public List<Integer> largura(Fila fila, int inicio, int destino, List<Integer> visitados){
+
+      fila.insereElemento(inicio);
+
+      while (!fila.filaVazia()){
+         int indiceAtual = fila.removeElemento();
+
+         if (!visitados.contains(indiceAtual)) {
+            visitados.add(indiceAtual);
+         }
+
+         if (indiceAtual == destino) {
+            System.out.print("\nBusca por largura caminho: ");
+            for (int i = 0; i < visitados.size(); i++) {
+               System.out.print(vertices[visitados.get(i)].rotulo);
+               if (i < visitados.size() - 1) {
+                  System.out.print(", ");
+               }
+            }
+
+            System.out.println("\n");
+
+            return visitados;
+         }
+
+         Vertice origem = vertices[indiceAtual];
+         Aresta atual = origem.inicio;
+
+         System.out.print("Fila: ");fila.imprimirFila();
+
+         while(atual != null){
+            if (!visitados.contains(atual.destino) && !fila.encontraElemento(atual.destino)){
+               fila.insereElemento(atual.destino);
+
+            }
+
+            atual = atual.proximo;
+         }
+
+         System.out.println("Visitando -> " + indiceAtual);
+         System.out.print("Fila: ");
+         fila.imprimirFila();
+         System.out.println("Visitados: " + visitados);
+         System.out.println("--------------------------");
+      }
+
+      System.out.println("Destino não encontrado!");
+
+      return null;
    }
 
    public void imprime() {
--- AtividadeDois/src/Main.java
+++ AtividadeDois/src/Main.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         ListaAdjacencia grafoLista = new ListaAdjacencia(4);
         MatrizAdjacencia grafoMatriz = new MatrizAdjacencia(4);
         int[] adjMatriz = new int[4];
+        int[] adj =  new int[4];
+        List<Integer> visitadosDistancia = new ArrayList<>();
+        List<Integer> visitadosPilha = new ArrayList<>();
+        List<Integer> visitadosLargura = new ArrayList<>();
+        Fila fila = new Fila();
         String[] rotulos = new String[]{"A","B","C","D"};
 
         for (int i = 0; i < rotulos.length; i++){
@@ -20,48 +28,45 @@ public class Main {
         grafoLista.criaAdjacencia(3, 1, 2);
         grafoLista.criaAdjacencia(1, 2, 7);
         grafoLista.criaAdjacencia(0,3,20);
-        grafoLista.criaAdjacencia(0,3,20);
+
 
         grafoLista.imprime();
+        grafoLista.profundidade(0,3,visitadosPilha);
+        grafoLista.largura(fila,0,3,visitadosLargura);
+        grafoLista.distancia(0, 2, visitadosDistancia);
+        grafoLista.saida();
 
-        grafoLista.adjacentes(0);
-
-        grafoLista.removeAdjacencia(0,2);
-
-        grafoLista.imprime();
-
-        grafoLista.adjacentes(0);
-
+        grafoLista.adjacentes(0, adj);
+        grafoLista.entrada();
 
 
-        System.out.println("Matriz: ");
 
-        grafoMatriz.criaAdjacencia(0, 1, 5);
-        grafoMatriz.criaAdjacencia(0, 2, 2);
-        grafoMatriz.criaAdjacencia(1, 3, 7);
-        grafoMatriz.criaAdjacencia(2, 3, 6);
-        grafoMatriz.criaAdjacencia(3, 1, 2);
-        grafoMatriz.criaAdjacencia(1, 2, 7);
-        grafoMatriz.criaAdjacencia(0,3,20);
+//        System.out.println("Matriz: ");
 
-        grafoMatriz.imprime();
-
-        grafoMatriz.adjacentes(0, adjMatriz);
-
-        grafoMatriz.removeAdjacencia(0,2);
-
-        grafoMatriz.imprime();
+//        grafoMatriz.criaAdjacencia(0, 1, 5);
+//        grafoMatriz.criaAdjacencia(0, 2, 2);
+//        grafoMatriz.criaAdjacencia(1, 3, 7);
+//        grafoMatriz.criaAdjacencia(2, 3, 6);
+//        grafoMatriz.criaAdjacencia(3, 1, 2);
+//        grafoMatriz.criaAdjacencia(1, 2, 7);
+//        grafoMatriz.criaAdjacencia(0,3,20);
 //
-        AlgoritmoWarshall algoritmoWarshall = new AlgoritmoWarshall();
-        boolean[][] fechamento = algoritmoWarshall.fechamento(grafoLista);
-        algoritmoWarshall.imprime(fechamento, grafoLista);
-
+//        grafoMatriz.imprime();
 //
+//        grafoMatriz.adjacentes(0, adjMatriz);
 //
-       AlgoritmoDijkstra algoritmoDijkstra = new AlgoritmoDijkstra();
-       algoritmoDijkstra.dijkstra(grafoLista, 0, 3);
+//        grafoMatriz.removeAdjacencia(0,2);
+//
+//        grafoMatriz.imprime();
+//
+//        AlgoritmoWarshall algoritmoWarshall = new AlgoritmoWarshall();
+//        boolean[][] fechamento = algoritmoWarshall.fechamento(grafoLista);
+//        algoritmoWarshall.imprime(fechamento, grafoLista);
 
 
-//
+//       AlgoritmoDijkstra algoritmoDijkstra = new AlgoritmoDijkstra();
+//       algoritmoDijkstra.dijkstra(grafoLista, 0, 3);
+
+
     }
 }
